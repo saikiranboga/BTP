f77d1149de9b90565fe715e9561bcf98a735f32f
vivekvishwa <vivekvishwakarma001@gmail.com>
2014-01-26 22:35:02 +0530

    Changes: path variable for nr database added;

--- includes/path.php
+++ includes/path.php
@@ -3,4 +3,5 @@
 $blastBin = "/home/vivek/BTP/blast+/bin/";
 $blastQuery = "/home/vivek/RepoBTP/data/query";
 $blastDb = "/home/vivek/BTP/blast+/db/unannotated.fasta";
+$blastDbNr = "/home/vivek/BTP/blast+/db/nr";
 ?>
