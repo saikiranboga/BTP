9ac2a9b83742cace3e33b89f8dc392334f81f62f
vivekvishwa <vivekvishwakarma001@gmail.com>
2014-04-06 15:06:42 +0530

    Changes: - Updated .gitignore to ignore files in uploads and results folder;

--- includes/path.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Global vars for path to directories */
-$blastBin = "/path/to/blast+/binaries/";
-$blastQuery = "/path/to/blastquery";
-$blastDb = "/path/to/blast+/db/unannotated.fasta";
-$blastDbNr = "/path/to/blast+/db/nr";
-?>
