f934957f3ae8925e97015385d1b11867624d93ad
vivekvishwa <vivekvishwakarma001@gmail.com>
2014-04-06 14:14:21 +0530

    Changes: Removed BTP-vivek subproject from folder and added /netbeans/private to .gitingnore

--- BTP-master-vivek/includes/path.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Global vars for path to directories */
-$blastBin = "/home/vivek/BTP/blast+/bin/";
-$blastQuery = "/home/vivek/RepoBTP/data/query";
-$blastDb = "/home/vivek/BTP/blast+/db/unannotated.fasta";
-$blastDbNr = "/home/vivek/BTP/blast+/db/nr";
-?>
